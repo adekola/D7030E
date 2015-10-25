f595ddefdc58a71df9326361dd10920f930283b9
Adekola <adekola.m.adebayo@gmail.com>
2015-10-25 14:16:32 +0100

    increased the fibo number to 1500

--- src/loadServer/ClientHandler.java
+++ src/loadServer/ClientHandler.java
@@ -37,7 +37,7 @@ public class ClientHandler extends Thread {
             // Decorate the streams so we can send characters
             // and not just bytes.  Ensure output is flushed
             // after every newline.
-            Fibonacci.getFibo(30);
-            System.err.println("Computation Complete");
+            Fibonacci.getFibo(1500);
+            System.out.println("Computation Complete");
     }
 }
